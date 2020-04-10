0d75677b5e6f85917e270212a8ebd2babb51be0a
IgorKey <Igor_Kuzminykh@epam.com>
2020-04-10 17:47:53 +0300

    transform project to JVM style (rewrite multiplatform part)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,29 @@
 import org.jetbrains.kotlin.gradle.tasks.*
 
 plugins {
-    `kotlin-multiplatform`
-    `kotlinx-serialization`
+    kotlin("jvm")
+    kotlin("plugin.serialization")
     `maven-publish`
     idea
 }
 
 apply(from = "gradle/git-version.gradle.kts")
-apply(from = "https://raw.githubusercontent.com/Drill4J/build-scripts/master/publish.gradle")
 
 repositories {
     mavenLocal()
     mavenCentral()
     jcenter()
-
 }
 
-kotlin {
-    jvm()
-    sourceSets {
-        named("commonMain") {
-            dependencies {
-                implementation("org.jetbrains.kotlin:kotlin-stdlib-common")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:0.20.0")
-            }
-        }
-        named("jvmMain") {
-            dependencies {
-                implementation(kotlin("stdlib-jdk8"))
-                implementation(kotlin("reflect"))
-                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
-                implementation("org.jetbrains.xodus:xodus-entity-store:1.3.91")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
-            }
-        }
-        named("jvmTest") {
-            dependencies {
-                implementation(kotlin("test-junit"))
-            }
-        }
-    }
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(kotlin("reflect"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.4")
+    implementation("org.jetbrains.xodus:xodus-entity-store:1.3.91")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:0.20.0")
+    testImplementation(kotlin("test-junit"))
 }
+
 
 
 tasks {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,16 @@
+@file:Suppress("UnstableApiUsage")
+
 rootProject.name = "kodux"
-enableFeaturePreview("GRADLE_METADATA")
+pluginManagement {
+    val kotlinVersion: String by settings
+    plugins {
+        kotlin("jvm") version kotlinVersion
+        kotlin("plugin.serialization") version kotlinVersion
+    }
+
+    repositories {
+        gradlePluginPortal()
+        maven(url = "https://oss.jfrog.org/artifactory/list/oss-release-local")
+        maven(url = "https://dl.bintray.com/kotlin/kotlinx/")
+    }
+}
