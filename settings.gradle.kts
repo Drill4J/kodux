3a04004c56248e53c86e0556c4d9d611333c81c1
Roma <roma.ivanov1999@mail.ru>
2021-02-05 13:43:08 +0300

    EPMDJ-4402 Add license header to all source files

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,11 +6,13 @@ apply(from = "$scriptUrl/maven-repo.settings.gradle.kts")
 pluginManagement {
     val kotlinVersion: String by settings
     val benchmarkVersion: String by settings
+    val licenseVersion: String by extra
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
         kotlin("plugin.allopen") version kotlinVersion
         id("kotlinx.benchmark") version benchmarkVersion
+        id("com.github.hierynomus.license") version licenseVersion
     }
 
     repositories {
