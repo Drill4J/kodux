99641e39a097541dabb0fa9090dd1628a1c5a6fe
Roma <roma.ivanov1999@mail.ru>
2021-02-05 13:43:08 +0300

    EPMDJ-4402 Add license header to all source files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
+import java.net.*
+
 plugins {
     kotlin("jvm")
     kotlin("plugin.serialization")
+    id("com.github.hierynomus.license")
     `maven-publish`
 }
 
@@ -50,3 +53,17 @@ publishing {
         }
     }
 }
+
+val licenseFormatSettings by tasks.registering(com.hierynomus.gradle.license.tasks.LicenseFormat::class) {
+    source = fileTree(project.projectDir).also {
+        include("**/*.kt", "**/*.java", "**/*.groovy")
+        exclude("**/.idea")
+    }.asFileTree
+    headerURI = URI("https://raw.githubusercontent.com/Drill4J/drill4j/develop/COPYRIGHT")
+}
+
+license {
+    skipExistingHeaders = true
+}
+
+tasks["licenseFormat"].dependsOn(licenseFormatSettings)
