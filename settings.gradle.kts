2b85adbb49ee27a24f1c52b4003a9727f0dfd3a5
LikhanovMaxim <LikhanovMaxim@yandex.ru>
2021-04-15 17:12:29 +0300

    EPMDJ-6884 Weak string pool, Count in long removed benchmarks

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,11 @@ apply(from = "$scriptUrl/maven-repo.settings.gradle.kts")
 
 pluginManagement {
     val kotlinVersion: String by settings
-    val benchmarkVersion: String by settings
     val licenseVersion: String by extra
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion
         kotlin("plugin.allopen") version kotlinVersion
-        id("kotlinx.benchmark") version benchmarkVersion
         id("com.github.hierynomus.license") version licenseVersion
     }
 
@@ -19,5 +17,3 @@ pluginManagement {
         maven("https://dl.bintray.com/kotlin/kotlinx")
     }
 }
-
-include(":benchmarks")
